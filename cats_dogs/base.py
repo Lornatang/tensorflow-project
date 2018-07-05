25ae6101d5011a08e4e644ffeee91a01d301dd04
shiyipaisizuo <shiyipaisizuo@gmail.com>
2018-07-05 18:53:34 +0800

    Fix major bugs

--- cats_dogs/base.py
+++ cats_dogs/base.py
@@ -1,96 +1,93 @@
+"""
+    Training data and validation accuracy.
+"""
+
+# Author: Changyu Liu <Shiyipaisizuo@gmail.com>
+# Last modified: 2018-07-05
+# LICENSE: MIT
+
 import os
 import numpy as np
 import tensorflow as tf
 from PIL import Image
 
-import train_test_split
+import input_data
 import model
 
-N_CLASSES = 2
-IMG_W = 208  # resize the image, if the input image is too large, training will be very slow.
+
+N_CLASSES = 2  # dogs and cats
+IMG_W = 208  # resize the image, if the input image is too large, training will be very slow
 IMG_H = 208
-RATIO = 0.2  # take 20% of dataset as validation data
-BATCH_SIZE = 64
+BATCH_SIZE = 16
 CAPACITY = 2000
-MAX_STEP = 6000  # with current parameters, it is suggested to use MAX_STEP>10k
-learning_rate = 0.0001  # with current parameters, it is suggested to use learning rate<0.0001
+MAX_STEP = 15000
+# with current parameters, it is suggested to use learning rate<0.0001
+learning_rate = 0.0001
 
 
 def run_training():
-    # you need to change the directories to yours.
+    # Set there directories .
     train_dir = './data/train/'
     logs_train_dir = './logs/train/'
-    logs_val_dir = './logs/val/'
 
-    train, train_label, val, val_label = train_test_split.get_files(train_dir, RATIO)
-    train_batch, train_label_batch = train_test_split.get_batch(train,
-                                                                train_label,
-                                                                IMG_W,
-                                                                IMG_H,
-                                                                BATCH_SIZE,
-                                                                CAPACITY)
-    val_batch, val_label_batch = train_test_split.get_batch(val,
-                                                            val_label,
-                                                            IMG_W,
-                                                            IMG_H,
-                                                            BATCH_SIZE,
-                                                            CAPACITY)
+    train, train_label = input_data.get_files(train_dir)
 
-    x = tf.placeholder(tf.float32, shape=[BATCH_SIZE, IMG_W, IMG_H, 3])
-    y_ = tf.placeholder(tf.int16, shape=[BATCH_SIZE])
+    train_batch, train_label_batch = input_data.get_batch(train,
+                                                          train_label,
+                                                          IMG_W,
+                                                          IMG_H,
+                                                          BATCH_SIZE,
+                                                          CAPACITY)
+    train_logits = model.inference(train_batch, BATCH_SIZE, N_CLASSES)
+    train_loss = model.losses(train_logits, train_label_batch)
+    train_op = model.training(train_loss, learning_rate)
+    train__acc = model.evaluation(train_logits, train_label_batch)
 
-    logits = model.inference(x, BATCH_SIZE, N_CLASSES)
-    loss = model.losses(logits, y_)
-    acc = model.evaluation(logits, y_)
-    train_op = model.training(loss, learning_rate)
+    summary_op = tf.summary.merge_all()
+    sess = tf.Session()
+    train_writer = tf.summary.FileWriter(logs_train_dir, sess.graph)
+    saver = tf.train.Saver()
 
-    with tf.Session() as sess:
-        saver = tf.train.Saver()
-        sess.run(tf.global_variables_initializer())
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    sess.run(tf.global_variables_initializer())
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
-        summary_op = tf.summary.merge_all()
-        train_writer = tf.summary.FileWriter(logs_train_dir, sess.graph)
-        val_writer = tf.summary.FileWriter(logs_val_dir, sess.graph)
+    try:
+        for step in np.arange(MAX_STEP):
+            if coord.should_stop():
+                break
+            _, tra_loss, tra_acc = sess.run([train_op, train_loss, train__acc])
 
-        try:
-            for step in np.arange(MAX_STEP):
-                if coord.should_stop():
-                    break
+            if step % 50 == 0:
+                print(
+                    "Step %d, ".format(step),
+                    "train loss = {:.2f}, ".format(tra_loss),
+                    "train accuracy = {:.2f}%".format(tra_acc * 100.0))
+                summary_str = sess.run(summary_op)
+                train_writer.add_summary(summary_str, step)
 
-                tra_images, tra_labels = sess.run([train_batch, train_label_batch])
-                _, tra_loss, tra_acc = sess.run([train_op, loss, acc],
-                                                feed_dict={x: tra_images, y_: tra_labels})
-                if step % 50 == 0:
-                    print("Step {}, train loss = {:.2f}, "
-                          "train accuracy = {:.2f}%.".format(step, tra_loss, tra_acc * 100.0))
-                    summary_str = sess.run(summary_op)
-                    train_writer.add_summary(summary_str, step)
+            if step % 2000 == 0 or (step + 1) == MAX_STEP:
+                checkpoint_path = os.path.join(logs_train_dir, "model.ckpt")
+                saver.save(sess, checkpoint_path, global_step=step)
 
-                if step % 200 == 0 or (step + 1) == MAX_STEP:
-                    val_images, val_labels = sess.run([val_batch, val_label_batch])
-                    val_loss, val_acc = sess.run([loss, acc],
-                                                 feed_dict={x: val_images, y_: val_labels})
-                    print("Step {}, val loss = {:.2f}, val accuracy = {:.2f}%.".format(step, val_loss, val_acc * 100.0))
-                    summary_str = sess.run(summary_op)
-                    val_writer.add_summary(summary_str, step)
+    except tf.errors.OutOfRangeError:
+        print("Done training -- epoch limit reached")
+    finally:
+        coord.request_stop()
 
-                if step % 2000 == 0 or (step + 1) == MAX_STEP:
-                    checkpoint_path = os.path.join(logs_train_dir, 'model.ckpt')
-                    saver.save(sess, checkpoint_path, global_step=step)
-
-        except tf.errors.OutOfRangeError:
-            print("Done training -- epoch limit reached")
-        finally:
-            coord.request_stop()
-        coord.join(threads)
+    coord.join(threads)
+    sess.close()
 
 
 def get_one_image(train):
     """
         Randomly pick one image from training data
-    Return: ndarray
+    ====================
+    Args:
+        train: train data
+    ====================
+    Return:
+        image
     """
     n = len(train)
     ind = np.random.randint(0, n)
@@ -99,7 +96,6 @@ def get_one_image(train):
     image = Image.open(img_dir)
     image = image.resize([208, 208])
     image = np.array(image)
-
     return image
 
 
@@ -110,7 +106,7 @@ def evaluate_one_image():
 
     # you need to change the directories to yours.
     train_dir = './data/train/'
-    train, train_label = train_test_split.get_files(train_dir)
+    train, train_label = input_data.get_files(train_dir)
     image_array = get_one_image(train)
 
     with tf.Graph().as_default():
@@ -120,12 +116,11 @@ def evaluate_one_image():
         image = tf.cast(image_array, tf.float32)
         image = tf.image.per_image_standardization(image)
         image = tf.reshape(image, [1, 208, 208, 3])
-
         logits = model.inference(image, batch_size, n_classes)
 
         logits = tf.nn.softmax(logits)
 
-        x = tf.placeholder(tf.float32, shape=[208, 208, 3])
+        X = tf.placeholder(tf.float32, shape=[208, 208, 3])
 
         # you need to change the directories to yours.
         logs_train_dir = './logs/train/'
@@ -137,15 +132,18 @@ def evaluate_one_image():
             print("Reading checkpoints...")
             ckpt = tf.train.get_checkpoint_state(logs_train_dir)
             if ckpt and ckpt.model_checkpoint_path:
-                global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+                global_step = ckpt.model_checkpoint_path.split(
+                    '/')[-1].split('-')[-1]
                 saver.restore(sess, ckpt.model_checkpoint_path)
                 print("Loading success, global_step is %s".format(global_step))
             else:
                 print("No checkpoint file found")
 
-            prediction = sess.run(logits, feed_dict={x: image_array})
+            prediction = sess.run(logits, feed_dict={X: image_array})
             max_index = np.argmax(prediction)
             if max_index == 0:
-                print("This is a cat with possibility {:.6f}.".format(prediction[:, 0]))
+                print("This is a cat with possibility {:.6f}".format(
+                    prediction[:, 0]))
             else:
-                print("This is a dog with possibility {:.6f}.".format(prediction[:, 1]))
+                print("This is a dog with possibility {:.6f}".format(
+                    prediction[:, 1]))
--- cats_dogs/input_data.py
+++ cats_dogs/input_data.py
@@ -11,14 +11,21 @@ import numpy as np
 import os
 
 
-# you need to change this to your data directory
+# data directory
 train_dir = './data/train/'
+BATCH_SIZE = 2
+CAPACITY = 256
+IMG_W = 208
+IMG_H = 208
 
 
 def get_files(file_dir):
     """
+        Get the data you need to train
+    ====================
     Args:
         file_dir: file directory
+    ====================
     Returns:
         list of images and labels
     """
@@ -34,51 +41,48 @@ def get_files(file_dir):
         else:
             dogs.append(file_dir + file)
             label_dogs.append(1)
-    print('There are %d cats\nThere are %d dogs' % (len(cats), len(dogs)))
+    print("There are {} cats\nThere are {} dogs".format(len(cats), len(dogs)))
 
-    image_list = np.hstack((cats, dogs))
-    label_list = np.hstack((label_cats, label_dogs))
+    img_list = np.hstack((cats, dogs))
+    labels_list = np.hstack((label_cats, label_dogs))
 
-    temp = np.array([image_list, label_list])
+    temp = np.array([img_list, labels_list])
     temp = temp.transpose()
     np.random.shuffle(temp)
 
-    image_list = list(temp[:, 0])
-    label_list = list(temp[:, 1])
-    label_list = [int(i) for i in label_list]
+    img_list = list(temp[:, 0])
+    labels_list = list(temp[:, 1])
+    labels_list = [int(k) for k in labels_list]
 
-    return image_list, label_list
+    return img_list, labels_list
 
 
-# %%
-
-def get_batch(image, label, image_W, image_H, batch_size, capacity):
+def get_batch(image, labels, image_W, image_H, batch_size, capacity):
     """
+        Get the batch training size
+    ====================
     Args:
         image: list type
-        label: list type
+        labels: list type
         image_W: image width
         image_H: image height
         batch_size: batch size
         capacity: the maximum elements in queue
+    ====================
     Returns:
         image_batch: 4D tensor [batch_size, width, height, 3], dtype=tf.float32
         label_batch: 1D tensor [batch_size], dtype=tf.int32
     """
 
     image = tf.cast(image, tf.string)
-    label = tf.cast(label, tf.int32)
+    labels = tf.cast(labels, tf.int32)
 
     # make an input queue
-    input_queue = tf.train.slice_input_producer([image, label])
+    input_queue = tf.train.slice_input_producer([image, labels])
 
-    label = input_queue[1]
+    labels = input_queue[1]
     image_contents = tf.read_file(input_queue[0])
     image = tf.image.decode_jpeg(image_contents, channels=3)
-
-    ######################################
-    # data argumentation should go to here
-    ######################################
 
     image = tf.image.resize_image_with_crop_or_pad(image, image_W, image_H)
 
@@ -86,29 +90,16 @@ def get_batch(image, label, image_W, image_H, batch_size, capacity):
     # comment the following line.
     image = tf.image.per_image_standardization(image)
 
-    image_batch, label_batch = tf.train.batch([image, label],
-                                              batch_size=batch_size,
-                                              num_threads=64,
-                                              capacity=capacity)
+    img_batch, labels_batch = tf.train.batch([image, labels],
+                                             batch_size=batch_size,
+                                             num_threads=64,
+                                             capacity=capacity)
 
-    image_batch, label_batch = tf.train.shuffle_batch([image, label],
-                                                      batch_size=BATCH_SIZE,
-                                                      num_threads=64,
-                                                      capacity=CAPACITY,
-                                                      min_after_dequeue=CAPACITY - 1)
+    labels_batch = tf.reshape(labels_batch, [batch_size])
+    img_batch = tf.cast(img_batch, tf.float32)
 
-    label_batch = tf.reshape(label_batch, [batch_size])
-    image_batch = tf.cast(image_batch, tf.float32)
+    return img_batch, labels_batch
 
-    return image_batch, label_batch
-
-
-BATCH_SIZE = 2
-CAPACITY = 256
-IMG_W = 208
-IMG_H = 208
-
-train_dir = './data/train/'
 
 image_list, label_list = get_files(train_dir)
 image_batch, label_batch = get_batch(
@@ -126,11 +117,11 @@ with tf.Session() as sess:
 
             # just test one batch
             for j in np.arange(BATCH_SIZE):
-                print('label: %d' % label[j])
+                print("label: {}".format(label[j]))
             i += 1
 
     except tf.errors.OutOfRangeError:
-        print('done!')
+        print("Done!")
     finally:
         coord.request_stop()
     coord.join(threads)
