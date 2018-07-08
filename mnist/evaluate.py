67912a5b1427affed91da3cf999c62d2fb608cac
shiyipaisizuo <shiyipaisizuo@gmail.com>
2018-07-08 22:58:14 +0800

    This is best!

---
+++ mnist/evaluate.py
@@ -0,0 +1,10 @@
+import base
+import tensorflow as tf
+
+
+def main(argv=None):
+    base.evaluate()
+
+
+if __name__ == '__main__':
+    tf.app.run()
---
+++ mnist/train.py
@@ -0,0 +1,10 @@
+import base
+import tensorflow as tf
+
+
+def main(argv=None):
+    base.training()
+
+
+if __name__ == '__main__':
+    tf.app.run()
