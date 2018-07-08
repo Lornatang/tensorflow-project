27e893cc2ca20e4a3bcb0b17eb30031e1f918789
shiyipaisizuo <shiyipaisizuo@gmail.com>
2018-07-08 20:50:07 +0800

    I do not modify fuck this code.!

--- mnist/main.py
+++ mnist/main.py
@@ -19,28 +19,25 @@ def bias_variable(shape):
 
 
 # 定义卷积层、池化层的函数
-def conv2d(x, W):
+def conv2d(x, w):
     # conv2d是二维卷积，x是输入，W是参数，如[5,5,1,32]前面两个是尺寸
     # 第三个是channel，单色是1，RGB是3，第4个是卷积核数量
     # SAME是保持输入输出尺寸一样
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+    return tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding='SAME')
 
 
 def max_pool_2x2(x):
     # 这里都是横竖降成一半，每二个取灰度最大的那个，保留最显著的特征
-    return tf.nn.max_pool(
-        x, ksize=[
-            1, 2, 2, 1], strides=[
-            1, 2, 2, 1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
 # 因为卷积会利用到空间信息，所以要将1D的转化为2D的，所以要把数据给的1*784转为28*28d的
 # 图片最终尺寸为[-1,28,28,1]，-1是样本不固定的意思，最后的1是通道数量
 # None表示不限输入维度
-x = tf.placeholder(tf.float32, [None, 784])
-y_ = tf.placeholder(tf.float32, [None, 10])
+X = tf.placeholder(tf.float32, [None, 784])
+y = tf.placeholder(tf.float32, [None, 10])
 
-x_image = tf.reshape(x, [-1, 28, 28, 1])
+x_image = tf.reshape(X, [-1, 28, 28, 1])
 
 # 定义第一个卷积层
 W_conv1 = weight_variable([5, 5, 1, 32])
@@ -77,7 +74,7 @@ y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 # 定义损失函数，使用优化器Adam，学习率设为1e-4
 # 这里的reduce_mean就是普通的均值，不传axis就默认全局均值
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv),
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y * tf.log(y_conv),
                                               reduction_indices=[1]))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
@@ -85,7 +82,7 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 # tf.argmax就是从一个Tensor中找到值最大的序号
 # equal判断相等就返回True
 # tf.cast把correct_prediction的bool转成float32，e.g.[1,0,1,0]
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # 开始训练，初始化所有参数
@@ -97,13 +94,13 @@ with tf.Session() as sess:
         if i % 100 == 0:
             # t.eval()时，等价于：tf.get_default_session().run(t)
             # accuracy run不影响训练
-            train_accuracy = accuracy.eval(feed_dict={x: batch[0],
-                                                      y_: batch[1],
+            train_accuracy = accuracy.eval(feed_dict={X: batch[0],
+                                                      y: batch[1],
                                                       keep_prob: 1.0})
             print("step %d, training accuracy %g" % (i, train_accuracy))
     # train_step run就会改变值了
-        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+        train_step.run(feed_dict={X: batch[0], y: batch[1], keep_prob: 0.5})
 
-    print("test accuracy :%g" % accuracy.eval(feed_dict={x: mnist.test.images,
-                                                         y_: mnist.test.labels,
+    print("test accuracy :%g" % accuracy.eval(feed_dict={X: mnist.test.images,
+                                                         y: mnist.test.labels,
                                                          keep_prob: 1.0}))
