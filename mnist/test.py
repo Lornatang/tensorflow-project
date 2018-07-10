9c0de3b9b01d13259c402629a661abca3727a9f1
shiyipaisizuo <shiyipaisizuo@gmail.com>
2018-07-10 10:21:12 +0800

    add 2.png to test

---
+++ mnist/test.py
@@ -0,0 +1,18 @@
+"""
+    Test my number.
+"""
+
+# Author: Changyu Liu <Shiyipaisizuo@gmail.com>
+# Last modified: 2018-07-010
+# LICENSE: MIT
+
+import base
+import tensorflow as tf
+
+
+def main():
+    base.test()
+
+
+if __name__ == '__main__':
+    tf.app.run()
