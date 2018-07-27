ca6d7a6a412b55661e069d639272aba0f0e12551
Lornatang <shiyipaisizuo@gmail.com>
2018-07-28 00:09:22 +0800

    add keras code

--- keras/example/mnist_sample.py
+++ keras/example/mnist_sample.py
@@ -1,28 +1,34 @@
+"""
+# author: shiyipaisizuo
+# contact: shiyipaisizuo@gmail.com
+# file: mnist_sample.py
+# time: 2018/7/27 23:22
+# license: MIT
+"""
 import keras
 from keras.datasets import mnist
 from keras.layers import Dense, Dropout
 from keras.models import Sequential
-from keras.optimizers import RMSprop
+from keras.optimizers import Adam
 
 batch_size = 128
 num_classes = 10
-epochs = 20
+epochs = 5
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data(path='/tmp/mnist/')
+(train_x, train_y), (test_x, test_y) = mnist.load_data()
 
-x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+train_x = train_x.reshape(60000, 784)
+test_x = test_x.reshape(10000, 784)
+train_x = train_x.astype('float32')
+test_x = test_x.astype('float32')
+train_x /= 255
+test_x /= 255
+print(train_x.shape[0], 'train samples')
+print(test_x.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+train_y = keras.utils.to_categorical(train_y, num_classes)
+test_y = keras.utils.to_categorical(test_y, num_classes)
 
 model = Sequential()
 model.add(Dense(512, activation='relu', input_shape=(784,)))
@@ -34,14 +40,14 @@ model.add(Dense(num_classes, activation='softmax'))
 model.summary()
 
 model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(),
+              optimizer=Adam(),
               metrics=['accuracy'])
 
-history = model.fit(x_train, y_train,
+history = model.fit(train_x, train_y,
                     batch_size=batch_size,
                     epochs=epochs,
                     verbose=1,
-                    validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
+                    validation_data=(test_x, test_y))
+score = model.evaluate(test_x, test_y, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
