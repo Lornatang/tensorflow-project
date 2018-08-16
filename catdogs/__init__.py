2c735b39471e6fb31d0cd4010a6bb85d766b4739
Lornatang <shiyipaisizuo@gmail.com>
2018-08-16 16:17:11 +0800

    optimizer parameter update torch 0.4.1

---
+++ catdogs/__init__.py
@@ -0,0 +1,9 @@
+"""
+# author: shiyipaisizuo
+# contact: shiyipaisizuo@gmail.com
+# file: __init__.py
+# time: 2018/8/16 15:30
+# license: MIT
+"""
+
+__all__ = ['utils.py']
---
+++ catdogs/train.py
@@ -0,0 +1,71 @@
+"""
+# author: shiyipaisizuo
+# contact: shiyipaisizuo@gmail.com
+# file: train.py
+# time: 2018/8/16 16:10
+# license: MIT
+"""
+
+
+import tensorflow as tf
+from tensorflow.contrib import slim
+import numpy as np
+
+from .utils import LoadDataset
+
+
+def AlexNet(x_input):
+    with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                        activation_fn=tf.nn.relu,
+                        weights_initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.01),
+                        weights_regularizer=slim.l2_regularizer(0.0005)):
+        # layer 1
+        net = slim.conv2d(x_input, 96, [11, 11], padding="VALID", stride=4, scope="conv_1")
+        net = tf.nn.local_response_normalization(net, depth_radius=5, bias=2, alpha=0.0001, beta=0.75)
+        net = slim.max_pool2d(net, [3, 3], 2, scope="pool_1")
+        # layer 2
+        net = slim.conv2d(net, 256, [5, 5], scope="conv_2")
+        net = tf.nn.local_response_normalization(net, depth_radius=5, bias=2, alpha=0.0001, beta=0.75)
+        net = slim.max_pool2d(net, [3, 3], 2, scope="pool_2")
+        # layer 3 4
+        net = slim.repeat(net, 2, slim.conv2d, 384, [3, 3], scope="conv_3_4")
+        # layer 5
+        net = slim.conv2d(net, 256, [3, 3], scope="conv_5")
+        net = slim.max_pool2d(net, [3, 3], 2, scope="pool_5")
+        # flatten
+        net = slim.flatten(net, scope="flatten")
+        # layer 6
+        net = slim.fully_connected(net, 4096, scope="fc_6")
+        net = slim.dropout(net, keep_prob=0.5)
+        # layer 7
+        net = slim.fully_connected(net, 4096, scope="fc_7")
+        net = slim.dropout(net, keep_prob=0.5)
+        # output
+        net = slim.fully_connected(net, 2, scope="output", activation_fn=None)
+
+        return net
+
+
+# 构建输入、输出、loss、train_op等
+x_inputs = tf.placeholder(shape=[None, 128, 128, 3], dtype=tf.float32)
+y_labels = tf.placeholder(shape=[None, 2], dtype=tf.float32)
+
+train_datasets = LoadDataset("../data/catdog/train")
+
+with tf.Session() as sess:
+    tf.run(tf.global_variables_initializer())
+
+    # model
+    predicts = AlexNet(x_inputs)
+    # loss function
+    loss = tf.losses.softmax_cross_entropy(y_labels, predicts)
+    # train op
+    optimizer = tf.train.Adam(learning_rate=0.01, momentum=0.9).minimize(loss)
+    # accuracy
+    correct_prediction = tf.equal(tf.argmax(predicts, 1), tf.argmax(y_labels, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    for epoch in range(20):
+        images, labels = train_datasets.reads(batch_size=128)
+        accuracy, optimizer, loss = sess.run([accuracy, optimizer, loss], feed_dict={x_inputs: images, y_labels: labels})
+        print(f"Acc: {accuracy} Loss: {loss}")
