89290596751cbe4f3aa562e92ee463a4925ff3d5
Lornatang <shiyipaisizuo@gmail.com>
2018-09-11 17:14:41 +0800

    Rewrite most of the code to use the argparse parameter format

--- MNIST/mnist.py
+++ MNIST/mnist.py
@@ -1,30 +1,40 @@
+"""
+# author: shiyipaisizuo
+# contact: shiyipaisizuo@gmail.com
+# file: mnist.py
+# time: 2018/9/10 15:00
+# license: MIT
+"""
+
+import argparse
+
 import tensorflow as tf
-import tensorflow.examples.tutorials.mnist.input_data as input_data
-
-
+from tensorflow.examples.tutorials.mnist import input_data
+# log
 tf.logging.set_verbosity(tf.logging.DEBUG)
 
-mnist = input_data.read_data_sets("/tmp/mnist/", one_hot=True)  # Load data
+data = input_data.read_data_sets("../data/MNIST/MNIST/", one_hot=True)  # Load data
 
 # Parameters
-learning_rate = 0.001
-MAX_STEP = 1000
-batch_size = 128
-display_step = 1
-
+parser = argparse.ArgumentParser("Tensorflow mnist")
+parser.add_argument('--learning_rate', default=0.001)
+parser.add_argument('--epoch', default=100)
+parser.add_argument('--batch_size', default=128)
+parser.add_argument('--dis_epoch', default=2)
 # Network Parameters
-n_input = 784  # MNIST data input(image shape = [28,28])
-n_classes = 10  # MNIST total classes (0-9digits)
-dropout = 0.75  # probability to keep units
+parser.add_argument('--img_size', default=784)
+parser.add_argument('--classes', default=10)
+parser.add_argument('--dropout', default=0.75)
+parser.add_argument('--model_path', default='../../models/tensorflow/MNIST/MNIST/MNIST.ckpt')
+args = parser.parse_args()
 
 # tf Graph input
-X = tf.placeholder(tf.float32, [None, n_input])
-y = tf.placeholder(tf.float32, [None, n_classes])
+X = tf.placeholder(tf.float32, [None, args.img_size])
+y = tf.placeholder(tf.float32, [None, args.classes])
 keep_prob = tf.placeholder(tf.float32)  # drop(keep probability)
 
+
 # Create model
-
-
 def conv2d(image, w, b):
     return tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(image, w, strides=[1, 1, 1, 1], padding='SAME'), b))
 
@@ -34,16 +44,16 @@ def max_pooling(image, k):
 
 
 weights = {
-    'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
-    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
-    'wd1': tf.Variable(tf.random_normal([7 * 7 * 64, 1024])),
-    'out': tf.Variable(tf.random_normal([1024, n_classes]))
+    'wc1': tf.Variable(tf.random_normal([3, 3, 1, 64])),
+    'wc2': tf.Variable(tf.random_normal([3, 3, 64, 128])),
+    'wd1': tf.Variable(tf.random_normal([7 * 7 * 128, 512])),
+    'out': tf.Variable(tf.random_normal([512, args.classes]))
 }
 biases = {
-    'bc1': tf.Variable(tf.random_normal([32])),
-    'bc2': tf.Variable(tf.random_normal([64])),
-    'bd1': tf.Variable(tf.random_normal([1024])),
-    'out': tf.Variable(tf.random_normal([n_classes]))
+    'bc1': tf.Variable(tf.random_normal([64])),
+    'bc2': tf.Variable(tf.random_normal([128])),
+    'bd1': tf.Variable(tf.random_normal([512])),
+    'out': tf.Variable(tf.random_normal([args.classes]))
 }
 
 
@@ -69,30 +79,37 @@ def conv_net(x, _weights, _biases, _dropout):
 # Construct model
 pred = conv_net(X, weights, biases, keep_prob)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=pred))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate=args.learning_rate).minimize(cost)
 
 # Evaluate model
 correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
+# create model save checkpoint
+saver = tf.train.Saver()
+
 with tf.Session() as sess:
+    # saver.restore(sess, args.model_path)
     sess.run(tf.global_variables_initializer())
-    for step in range(1, MAX_STEP):
-        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+    for step in range(1, args.epoch + 1):
+        batch_xs, batch_ys = data.train.next_batch(args.batch_size)
         sess.run(optimizer, feed_dict={X: batch_xs,
                                        y: batch_ys,
-                                       keep_prob: dropout})
-        if step % display_step == 0:
+                                       keep_prob: args.dropout})
+        if step % args.dis_epoch == 0 or step == 1:
             acc = sess.run(accuracy, feed_dict={X: batch_xs,
                                                 y: batch_ys,
                                                 keep_prob: 1.})
             loss = sess.run(cost, feed_dict={X: batch_xs,
                                              y: batch_ys,
                                              keep_prob: 1.})
-            print(f"Step {step} Minibatch Loss= {loss:.6f}, Training Accuracy= {acc:.5f}")
+            print(f"Step [{step}/{args.epoch}], "
+                  f"Loss: {loss:.6f}, "
+                  f"Acc: {acc:.5f}")
 
-        step += 1
+            saver.save(sess, args.model_path)
+
     print("Optimization Finished!")
-    print("Testing Accuracy: ", sess.run(accuracy, feed_dict={X: mnist.test.images[:256],
-                                                              y: mnist.test.labels[:256],
+    print("Testing Accuracy: ", sess.run(accuracy, feed_dict={X: data.test.images[:256],
+                                                              y: data.test.labels[:256],
                                                               keep_prob: 1.}))
