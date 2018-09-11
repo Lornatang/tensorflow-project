fff4a34aea494914477da0eafc2c5e9c47ced4eb
Lornatang <shiyipaisizuo@gmail.com>
2018-09-11 15:18:04 +0800

    Change all file structures to increase the number of papers

--- MNIST/fashionMnist.py
+++ MNIST/fashionMnist.py
@@ -6,20 +6,102 @@
 # license: MIT
 """
 
+import argparse
+
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-
+# log
 tf.logging.set_verbosity(tf.logging.DEBUG)
 
-mnist = input_data.read_data_sets("../data/mnist/", one_hot=True)  # Load data
+data = input_data.read_data_sets("../data/fashionMNIST/", one_hot=True)  # Load data
 
 # Parameters
-learning_rate = 0.001
-MAX_STEP = 1000
-batch_size = 128
-display_step = 1
-
+parser = argparse.ArgumentParser("Tensorflow mnist")
+parser.add_argument('--learning_rate', default=0.0001)
+parser.add_argument('--epoch', default=100)
+parser.add_argument('--batch_size', default=128)
+parser.add_argument('--dis_epoch', default=2)
 # Network Parameters
-n_input = 784  # MNIST data input(image shape = [28,28])
-n_classes = 10  # MNIST total classes (0-9digits)
-dropout = 0.75  # probability to keep units
+parser.add_argument('--img_size', default=784)
+parser.add_argument('--classes', default=10)
+parser.add_argument('--dropout', default=0.75)
+
+# tf Graph input
+X = tf.placeholder(tf.float32, [None, n_input])
+y = tf.placeholder(tf.float32, [None, n_classes])
+keep_prob = tf.placeholder(tf.float32)  # drop(keep probability)
+
+# Create model
+
+
+def conv2d(image, w, b):
+    return tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(image, w, strides=[1, 1, 1, 1], padding='SAME'), b))
+
+
+def max_pooling(image, k):
+    return tf.nn.max_pool(image, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')
+
+
+weights = {
+    'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+    'wd1': tf.Variable(tf.random_normal([7 * 7 * 64, 1024])),
+    'out': tf.Variable(tf.random_normal([1024, n_classes]))
+}
+biases = {
+    'bc1': tf.Variable(tf.random_normal([32])),
+    'bc2': tf.Variable(tf.random_normal([64])),
+    'bd1': tf.Variable(tf.random_normal([1024])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+
+def conv_net(x, _weights, _biases, _dropout):
+    # Layer 1
+    x = tf.reshape(x, [-1, 28, 28, 1])
+    conv1 = conv2d(x, _weights['wc1'], _biases['bc1'])
+    conv1 = max_pooling(conv1, k=2)
+    conv1 = tf.nn.dropout(conv1, keep_prob=_dropout)
+    # Layer 2
+    conv2 = conv2d(conv1, _weights['wc2'], _biases['bc2'])
+    conv2 = max_pooling(conv2, k=2)
+    conv2 = tf.nn.dropout(conv2, keep_prob=_dropout)
+    # Fully Connected
+    dense1 = tf.reshape(conv2, [-1, _weights['wd1'].get_shape().as_list()[0]])
+    dense1 = tf.nn.relu(tf.add(tf.matmul(dense1, _weights['wd1']), _biases['bd1']))
+    dense1 = tf.nn.dropout(dense1, _dropout)
+    out = tf.add(tf.matmul(dense1, _weights['out']), _biases['out'])
+
+    return out
+
+
+# Construct model
+pred = conv_net(X, weights, biases, keep_prob)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=pred))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# Evaluate model
+correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for step in range(1, MAX_STEP):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        sess.run(optimizer, feed_dict={X: batch_xs,
+                                       y: batch_ys,
+                                       keep_prob: dropout})
+        if step % display_step == 0:
+            acc = sess.run(accuracy, feed_dict={X: batch_xs,
+                                                y: batch_ys,
+                                                keep_prob: 1.})
+            loss = sess.run(cost, feed_dict={X: batch_xs,
+                                             y: batch_ys,
+                                             keep_prob: 1.})
+            print(f"Step {step} Minibatch Loss= {loss:.6f}, Training Accuracy= {acc:.5f}")
+
+        step += 1
+    print("Optimization Finished!")
+    print("Testing Accuracy: ", sess.run(accuracy, feed_dict={X: mnist.test.images[:256],
+                                                              y: mnist.test.labels[:256],
+                                                              keep_prob: 1.}))
