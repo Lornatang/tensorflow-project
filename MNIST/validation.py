3412ec57da7b7d6ae35b5aad5e2b676f6b20db75
Lornatang <shiyipaisizuo@gmail.com>
2018-09-13 17:31:59 +0800

    update validation file

--- MNIST/validation.py
+++ MNIST/validation.py
@@ -5,86 +5,92 @@
 # time: 2018/9/11 19:02
 # license: MIT
 """
-
-from PIL import Image
 import argparse
+import gzip
+import os
+import urllib.request
 
+import numpy
+import sys
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-# log
-tf.logging.set_verbosity(tf.logging.DEBUG)
+from PIL import Image
 
-data = input_data.read_data_sets("../data/MNIST/MNIST/", one_hot=True)  # Load data
+SOURCE_URL = 'https://storage.googleapis.com/cvdf-datasets/mnist/'
+WORK_DIRECTORY = '../data/MNIST/MNIST/'
+IMAGE_SIZE = 28
+NUM_CHANNELS = 1
+PIXEL_DEPTH = 255
+NUM_LABELS = 10
+VALIDATION_SIZE = 5000  # Size of the validation set.
+SEED = 66478  # Set to None for random seed.
+BATCH_SIZE = 64
+NUM_EPOCHS = 10
+EVAL_BATCH_SIZE = 64
+EVAL_FREQUENCY = 100  # Number of steps between evaluations.
 
-# Parameters
-parser = argparse.ArgumentParser("Tensorflow mnist")
-parser.add_argument('--learning_rate', default=0.001)
-parser.add_argument('--epoch', default=100)
-parser.add_argument('--batch_size', default=128)
-parser.add_argument('--dis_epoch', default=2)
-# Network Parameters
-parser.add_argument('--img_size', default=784)
-parser.add_argument('--classes', default=10)
-parser.add_argument('--dropout', default=0.75)
-parser.add_argument('--model_path', default='../../models/tensorflow/MNIST/MNIST/MNIST.ckpt')
-args = parser.parse_args()
-
-# tf Graph input
-X = tf.placeholder(tf.float32, [None, args.img_size])
-y = tf.placeholder(tf.float32, [None, args.classes])
-keep_prob = tf.placeholder(tf.float32)  # drop(keep probability)
+FLAGS = None
 
 
-# Create model
-def conv2d(image, w, b):
-    return tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(image, w, strides=[1, 1, 1, 1], padding='SAME'), b))
+def data_type():
+    """Return the type of the activations, weights, and placeholder variables."""
+    if FLAGS.use_fp16:
+        return tf.float16
+    else:
+        return tf.float32
 
 
-def max_pooling(image, k):
-    return tf.nn.max_pool(image, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')
+def download(filename):
+    """Download the data from Yann's website, unless it's already here."""
+    if not tf.gfile.Exists(WORK_DIRECTORY):
+        tf.gfile.MakeDirs(WORK_DIRECTORY)
+    filepath = os.path.join(WORK_DIRECTORY, filename)
+    if not tf.gfile.Exists(filepath):
+        filepath, _ = urllib.request.urlretrieve(SOURCE_URL + filename, filepath)
+        with tf.gfile.GFile(filepath) as f:
+            size = f.size()
+        print(f"Successfully downloaded {filename} size {size} bytes.")
+
+    return filepath
 
 
-weights = {
-    'wc1': tf.Variable(tf.random_normal([3, 3, 1, 64])),
-    'wc2': tf.Variable(tf.random_normal([3, 3, 64, 128])),
-    'wd1': tf.Variable(tf.random_normal([7 * 7 * 128, 1024])),
-    'out': tf.Variable(tf.random_normal([1024, args.classes]))
-}
-biases = {
-    'bc1': tf.Variable(tf.random_normal([64])),
-    'bc2': tf.Variable(tf.random_normal([128])),
-    'bd1': tf.Variable(tf.random_normal([1024])),
-    'out': tf.Variable(tf.random_normal([args.classes]))
-}
+def extract_data(filename, num_images):
+    """Extract the images into a 4D tensor [image index, y, x, channels].
+
+    Values are rescaled from [0, 255] down to [-0.5, 0.5].
+    """
+    print('Extracting', filename)
+    with gzip.open(filename) as bytestream:
+        bytestream.read(16)
+        buf = bytestream.read(IMAGE_SIZE * IMAGE_SIZE * num_images * NUM_CHANNELS)
+        data = numpy.frombuffer(buf, dtype=numpy.uint8).astype(numpy.float32)
+        data = (data - (PIXEL_DEPTH / 2.0)) / PIXEL_DEPTH
+        data = data.reshape(num_images, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS)
+        return data
 
 
-def conv_net(x, _weights, _biases, _dropout):
-    # Layer 1
-    x = tf.reshape(x, [-1, 28, 28, 1])
-    conv1 = conv2d(x, _weights['wc1'], _biases['bc1'])
-    conv1 = max_pooling(conv1, k=2)
-    conv1 = tf.nn.dropout(conv1, keep_prob=_dropout)
-    # Layer 2
-    conv2 = conv2d(conv1, _weights['wc2'], _biases['bc2'])
-    conv2 = max_pooling(conv2, k=2)
-    conv2 = tf.nn.dropout(conv2, keep_prob=_dropout)
-    # Fully Connected
-    dense1 = tf.reshape(conv2, [-1, _weights['wd1'].get_shape().as_list()[0]])
-    dense1 = tf.nn.relu(tf.add(tf.matmul(dense1, _weights['wd1']), _biases['bd1']))
-    dense1 = tf.nn.dropout(dense1, _dropout)
-    out = tf.add(tf.matmul(dense1, _weights['out']), _biases['out'])
-
-    return out
+def extract_labels(filename, num_images):
+    """Extract the labels into a vector of int64 label IDs."""
+    print('Extracting', filename)
+    with gzip.open(filename) as bytestream:
+        bytestream.read(8)
+        buf = bytestream.read(1 * num_images)
+        labels = numpy.frombuffer(buf, dtype=numpy.uint8).astype(numpy.int64)
+    return labels
 
 
-# Construct model
-pred = conv_net(X, weights, biases, keep_prob)
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=pred))
-optimizer = tf.train.AdamOptimizer(learning_rate=args.learning_rate).minimize(cost)
+def fake_data(num_images):
+    data = numpy.ndarray(shape=(num_images, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS),
+                         dtype=numpy.float32)
+    labels = numpy.zeros(shape=(num_images,), dtype=numpy.int64)
+    for image in range(num_images):
+        label = image % 2
+        data[image, :, :, 0] = label - 0.5
+        labels[image] = label
+    return data, labels
 
-# Evaluate model
-correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+def error_rate(predictions, labels):
+    return 100.0 - (100.0 * numpy.sum(numpy.argmax(predictions, 1) == labels) / predictions.shape[0])
 
 
 def imageprepare():
@@ -95,41 +101,109 @@ def imageprepare():
     file_name = '../data/MNIST/MNIST/test/2.jpg'  # 导入自己的图片地址
     # in terminal 'mogrify -format png *.jpg' convert jpg to png
     im = Image.open(file_name).convert('L')
-    tv = list(im.getdata())  # get pixel values
+    tva = list(im.getdata())  # get pixel values
 
     # normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
-    tva = [(255 - x) * 1.0 / 255.0 for x in tv]
     # print(tva)
     return tva
 
-    # Define the model (same as when creating the model file)
-
-
-result = imageprepare()
-
-init_op = tf.initialize_all_variables()
-
-"""
-Load the model2.ckpt file
-file is stored in the same directory as this python script is started
-Use the model to predict the integer. Integer is returend as list.
-
-Based on the documentatoin at
-https://www.tensorflow.org/versions/master/how_tos/variables/index.html
-"""
-saver = tf.train.Saver()
-
 
 def main(_):
-    tf.app.run(main=main, agrv=[sys.argv[0]])
+    if FLAGS.self_test:
+        print("Runing self-test.")
+        train_data, train_labels = fake_data(256)
+        validation_data, validation_labels = fake_data(EVAL_BATCH_SIZE)
+        test_data, test_labels = fake_data(EVAL_BATCH_SIZE)
+        num_epochs = 1
+    else:
+        # Get the data
+        # Get the data.
+        train_data_filename = download('train-images-idx3-ubyte.gz')
+        train_labels_filename = download('train-labels-idx1-ubyte.gz')
+        test_data_filename = download('t10k-images-idx3-ubyte.gz')
+        test_labels_filename = download('t10k-labels-idx1-ubyte.gz')
+
+        # Extract it into numpy arrays.
+        train_labels = extract_labels(train_labels_filename, 60000)
+
+        # Generate a validation set.
+        train_labels = train_labels[VALIDATION_SIZE:]
+
+    train_data_node = tf.placeholder(data_type(), shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
+
+    conv1_weights = tf.Variable(
+        tf.truncated_normal([5, 5, NUM_CHANNELS, 32],
+                            stddev=0.1,
+                            seed=SEED,
+                            dtype=data_type()))
+    conv1_biases = tf.Variable(tf.zeros([32], dtype=data_type()))
+    conv2_weights = tf.Variable(tf.truncated_normal(
+        [5, 5, 32, 64], stddev=0.1,
+        seed=SEED, dtype=data_type()))
+    conv2_biases = tf.Variable(tf.constant(0.1, shape=[64], dtype=data_type()))
+    fc1_weights = tf.Variable(  # fully connected, depth 512.
+        tf.truncated_normal([IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512],
+                            stddev=0.1,
+                            seed=SEED,
+                            dtype=data_type()))
+    fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=data_type()))
+    fc2_weights = tf.Variable(tf.truncated_normal([512, NUM_LABELS],
+                                                  stddev=0.1,
+                                                  seed=SEED,
+                                                  dtype=data_type()))
+    fc2_biases = tf.Variable(tf.constant(
+        0.1, shape=[NUM_LABELS], dtype=data_type()))
+
+    def model(data, train=False):
+        conv = tf.nn.conv2d(data, conv1_weights, strides=[1, 1, 1, 1], padding='SAME')
+        relu = tf.nn.relu(tf.nn.bias_add(conv, conv1_biases))
+        pool = tf.nn.max_pool(relu, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+                              padding='SAME')
+
+        conv = tf.nn.conv2d(pool, conv2_weights, strides=[1, 1, 1, 1], padding='SAME')
+        relu = tf.nn.relu(tf.nn.bias_add(conv, conv2_biases))
+        pool = tf.nn.max_pool(relu, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+                              padding='SAME')
+
+        pool_shape = pool.get_shape().as_list()
+        reshape = tf.reshape(pool, [pool_shape[0], pool_shape[1] * pool_shape[2] * pool_shape[3]])
+        hidden = tf.nn.relu(tf.matmul(reshape, fc1_weights) + fc1_biases)
+
+        if train:
+            hidden = tf.nn.dropout(hidden, 0.5, seed=SEED)
+        return tf.matmul(hidden, fc2_weights) + fc2_biases
+
+    logits = model(train_data_node, True)
+
+    train_prediction = tf.nn.softmax(logits)
+
+    # Create a local session to run the training.
+    with tf.Session() as sess:
+        # Run all the initializers to prepare the trainable parameters.
+        tf.global_variables_initializer().run()
+        print('Initialized!')
+        saver = tf.train.Saver()
+        saver.restore(sess, '../../models/tensorflow/MNIST/MNIST/mnist.ckpt')  # 这里使用了之前保存的模型参数
+        result = imageprepare()
+        prediction = tf.argmax(train_prediction, 1)
+        predint = prediction.eval(feed_dict={X: [result]}, session=sess)
+
+        print('recognize result:')
+        print(predint[0])
 
 
-with tf.Session() as sess:
-    saver.restore(sess, '../../models/tensorflow/MNIST/MNIST/MNIST.ckpt')  # 这里使用了之前保存的模型参数
-    # print ("Model restored.")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--use_fp16',
+        default=False,
+        help='Use half floats instead of full floats if True.',
+        action='store_true')
+    parser.add_argument(
+        '--self_test',
+        default=False,
+        action='store_true',
+        help='True if running a self test.')
 
-    prediction = tf.argmax(pred, 1)
-    predint = prediction.eval(feed_dict={X: [result], keep_prob: 1.0}, session=sess)
-
-    print('recognize result:')
-    print(predint[0])
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
